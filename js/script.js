3931f5c699a68b923f30dd54128b749cb88ee93d
LeonardoCeccarelli <leo.ceccarelli@tiscali.it>
2021-10-12 18:59:46 +0200

    set right code in js and add media queries

--- js/script.js
+++ js/script.js
@@ -1,32 +1,40 @@
-// Individuo e assegno con una variabile il contenitore dove andranno le card
-const cellContainer = document.getElementById("cell_container")
-
-// Creo la variabile row che inserisco all'interno del contenitore
-const cellRow = document.createElement("div")
-cellRow.classList.add("row", "row-cols-6", "gx-2", "gy-5")
-
-cellContainer.append(cellRow)
+// Individuo il contenitore delle card
+const cellRow = document.getElementById("cell_container")
 
 // Creo il ciclo per creare le card
 for (let i = 1; i <= 100; i++) {
     const cellCard = document.createElement("div")
 
-    cellCard.classList.add("col", "m-0")
+    cellCard.classList.add("my_col")
 
-    let bgColor = "bg-danger"
+    // sfondo card
+    let bgColor = "cell_bg_lightblue"
 
+    // testo dentro card
     let cellText = i
 
+    // creo le condizioni alla quale devono essere 
+    // sovrascritte le variabili sfondo e testo
+    if (i % 3 === 0) {
+        bgColor = "cell_bg_lightgreen"
+        cellText = "fizz"
+    }
+
+    if (i % 5 === 0) {
+        bgColor = "cell_bg_yellow"
+        cellText = "buzz"
+    }
+
+    if (i % 3 === 0 && i % 5 === 0) {
+        bgColor = "cell_bg_red"
+        cellText = "fizzbuzz"
+    }
+
     // Creo il contenuto delle card
-    cellCard.innerHTML += `<div class="ratio ratio-1x1 cell ${bgColor}>"
-                            <span>${cellText}</span>
+    cellCard.innerHTML += `<div class="ratio ratio-1x1 fw-bold cell ${bgColor}">
+                            ${cellText}
                            </div>`
 
-    console.log(cellCard)
-
     cellRow.append(cellCard)
+
 }
-
-
-
-
